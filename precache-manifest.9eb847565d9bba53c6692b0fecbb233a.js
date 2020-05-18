2bc43c2345085ffd6811fc481f181cd29c4bcba8
Kostya Peniyavskiy <kornelius9898@gmail.com>
2020-05-18 20:06:10 +0300

    Updates

---
+++ precache-manifest.9eb847565d9bba53c6692b0fecbb233a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e8fba223fa55b2b2acd924900e12570",
+    "url": "./index.html"
+  },
+  {
+    "revision": "41ff88cb97bfb0d56a30",
+    "url": "./static/css/main.db5e4f7b.chunk.css"
+  },
+  {
+    "revision": "9fdb488539fc6cad8275",
+    "url": "./static/js/2.b2b8d02b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.b2b8d02b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41ff88cb97bfb0d56a30",
+    "url": "./static/js/main.6d96bab4.chunk.js"
+  },
+  {
+    "revision": "55ecf25ea8d2bbd87954",
+    "url": "./static/js/runtime-main.80c0d849.js"
+  }
+]);
